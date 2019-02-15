0540eb6e9ec9c2d7a45f59e0437fe7d7ce92c4b6
makeitwayne17 <bryantliu17@gmail.com>
2019-02-14 22:15:36 -0800

    auto commit

--- routes/search.js
+++ routes/search.js
@@ -2,8 +2,9 @@
 /*
  * GET search for session page.
  */
+var sessions = require('../songs.json');
 
 exports.view = function(req, res){
-  res.render('search');
+  res.render('search', sessions);
 };
 
