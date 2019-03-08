7227a9a69a4f955e8e9acba2c614855bc7280dbe
makeitwayne17 <bryantliu17@gmail.com>
2019-03-07 17:23:40 -0800

    auto commit

--- app.js
+++ app.js
@@ -91,8 +91,9 @@ app.get('/helpHome', helpHome.view);
 
 app.get('/host', host.view);
 app.get('/hostSong', hostSong.view);
+app.get('/hostSongB', hostSongB.view);
 app.get('/hostSongA', hostSongAB.view);
-app.get('/hostSongB', hostSongAB.viewAlt);
+// app.get('/hostSongB', hostSongAB.viewAlt);
 
  app.get('/hostSongvA', hostSongA.view);          //IN use for AB Testing
  app.get('/hostSongvB', hostSongB.view);
