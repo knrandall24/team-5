037d47179fd71b246f64e47d258cfaddc3f3da08
makeitwayne17 <bryantliu17@gmail.com>
2019-03-07 17:20:59 -0800

    auto commit

--- public/js/host.js
+++ public/js/host.js
@@ -374,7 +374,7 @@ function postList(e){
     }
 
     // window.location.href = 'hostSong';
-    window.location.href = 'hostSongA';
+    window.location.href = 'hostSongB';
 
     // window.location.href = 'add';
 }
